26956caa16d8737a432c2b678b5d48fe7db5d495
taichomeiso <otaichididit@gmail.com>
2024-10-24 19:03:18 +0900

    覚えマスのjsのファイル名をより明確な名前に変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "memory-square"
+import "memory-square-result-display"
 import "result-display"
 
