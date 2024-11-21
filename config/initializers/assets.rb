d5b6956adf3f9a2cb3ab6334cb0131734cbe914e
taichomeiso <otaichididit@gmail.com>
2024-11-21 10:46:33 +0900

    色勝ちジャンケン微修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,9 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w[memory-square-tutorial-movie.mp4]
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.precompile += %w[
+  GameMaterial/GameScreenImage/color_rock_paper_sicissors_Image/**/*
+]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
